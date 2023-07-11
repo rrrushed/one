9a8a93edfa528b5a2cf8268145cf88b293800339
rrrushed <138280761+rrrushed@users.noreply.github.com>
2023-07-11 23:23:45 +0200

    Update gallery.js

--- public/gallery.js
+++ public/gallery.js
@@ -10,7 +10,7 @@ function updateImage() {
   const currentImage = images.shift(); // Take the first image from the list
   images.push(currentImage); // And add it back to the end of the list
 
-  imageElement.src = '/images 4/' + currentImage;
+  imageElement.src = '/images/' + currentImage;
 }
 
 fetch('/image-filenames')
